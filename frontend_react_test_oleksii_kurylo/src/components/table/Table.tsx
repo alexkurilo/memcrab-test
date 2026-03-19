6090a38e033237d59820abc00402d113f8eb19d6
Oleksii <oleksii.k@fieldfub.com>
2026-03-19 15:38:30 +0200

    created heatmap for cells closest in value to the selected one

--- frontend_react_test_oleksii_kurylo/src/components/table/Table.tsx
+++ frontend_react_test_oleksii_kurylo/src/components/table/Table.tsx
@@ -1,13 +1,23 @@
 import { useContext, useState } from "react";
 import { TableContext, type ICell} from "../../providers/TableProvider";
+import { ModalContext } from "../../providers/ModalProvider";
 
 import "./Table.css";
 
+type DiffAmountType = {
+  diff: number;
+  id: number;
+  amount: number;
+}
+
 export const Table = () => {
   const tableContext = useContext(TableContext);
+  const modalContext = useContext(ModalContext);
+
   const columnSum: number[] = [];
   const [hoveredIndexRow, setHoveredIndexRow] = useState<number | null>(null);
-
+  const [highlightCellsIds, setHighlightCellsIds] = useState<DiffAmountType[]>([]);
+  
   const onClickHandleToCell = (rowIndex: number, cellIndex: number) => {
     const tableData = JSON.parse(JSON.stringify(tableContext?.cells));
 
@@ -23,9 +33,40 @@ export const Table = () => {
           cell.rowSum = cell.rowSum + 1;
         });
       }
-      
     });
     tableContext?.updateCells(tableData);
+  };
+
+  const onHoverHandlerToCell = (selectedCell: ICell) => {
+    const resultSortedSet: DiffAmountType[] = [];
+    const limitLength = modalContext?.limits.quantity;
+    
+    resultSortedSet.push({
+      diff: 0,
+      id: selectedCell.id,
+      amount: selectedCell.amount,
+    });
+
+    tableContext?.cells.forEach((row) => {
+      row.forEach((cell) => {
+        if (limitLength && limitLength > 1 && cell.id !== selectedCell.id) {
+          const currentDiff: DiffAmountType = {
+            diff: Math.abs(cell.amount - selectedCell.amount),
+            id: cell.id,
+            amount: cell.amount,
+          };
+          if (resultSortedSet.length < limitLength) {
+            resultSortedSet.push(currentDiff)
+          } else {
+            if ((resultSortedSet[0].diff - currentDiff.diff) > 0) {
+              resultSortedSet.splice(0, 1, currentDiff);
+            }
+          }
+          resultSortedSet.sort((a, b) => (a.diff > b.diff ? -1 : 1));
+        }
+      })
+    });
+    setHighlightCellsIds(resultSortedSet);
   };
 
   return (
@@ -33,7 +74,7 @@ export const Table = () => {
       <table className="content-table">
         <thead>
           <tr key="tr-head">
-            {tableContext?.cells.length && tableContext?.cells[0].length && tableContext?.cells[0].map((cell, index) => {
+            {tableContext?.cells.length && tableContext?.cells[0].length && tableContext?.cells[0].map((_, index) => {
               return (
                 <>
                   {!index && <th key={`th-0${index}`} scope="col"/>}
@@ -62,8 +103,11 @@ export const Table = () => {
                           <>
                             {!indexCell && <th key={`th-${indexRow}.${indexCell}`} scope="col">{`Cell values M = ${indexRow + 1}`}</th>}
                             <td
+                              className={ highlightCellsIds.some((selectCell) => selectCell.id === cell.id) ? "highlight_limit" : ""}
                               key={`th-${indexRow}.${indexCell + 1}`}
                               onClick={() => onClickHandleToCell(indexRow, indexCell)}
+                              onMouseEnter={() => {onHoverHandlerToCell(cell)}}
+                              onMouseLeave={() => {setHighlightCellsIds([])}}
                             >
                               {hoveredIndexRow !==null && hoveredIndexRow === indexRow ? `${cell.percentageInRow} %` : cell.amount}
                             </td>
