aa78b3c432796b89ca2f6650fa010e02628e0104
Oleksii <oleksii.k@fieldfub.com>
2026-03-28 15:06:28 +0200

    build my test

--- frontend_react_test_oleksii_kurylo/src/constants.ts
+++
@@ -1,6 +0,0 @@
-export const minInputValue = 1;
-export const maxInputValue = 100;
-export const maxAmountGenerate = 999;
-
-export const hotHexColor = "#fa1100";
-export const coldHexColor = "#fbff00";
